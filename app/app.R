849dbd17ed1ee5d6eedbc3fcd5e24bbd878736da
deboonstra <daniel-boonstra@uiowa.edu>
2024-01-05 10:22:40 -0600

    Fixing directory listing for runApp

--- app/app.R
+++ app/app.R
@@ -1,22 +1,7 @@
-# Checking packages ####
-if (!("shiny" %in% installed.packages()[, 1])) {
-  utils::install.packages("shiny")
-}
-if (!("shinythemes" %in% installed.packages()[, 1])) {
-  utils::install.packages("shinythemes")
-}
-if (!("remotes" %in% installed.packages()[, 1])) {
-  utils::install.packages("remotes")
-}
-if (!("agstudy1app" %in% installed.packages()[, 1])) {
-  remotes::install_github("deboonstra/agstudy1app")
-}
-
-
 # Loading libraries ####
 library(shiny)
 library(shinythemes)
 library(agstudy1app)
 
 # Running app ####
-shiny::runApp(appDir = "app")
+shiny::runApp(appDir = ".")
